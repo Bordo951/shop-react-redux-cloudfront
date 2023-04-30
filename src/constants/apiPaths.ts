97ea9ac99a0bb55a7cc6b28f17311f8a5637d05e
Irina Selivanova <irina_selivanova@epam.com>
2023-04-30 22:12:58 +0300

    fix: remove redundant import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,6 @@
 const API_PATHS = {
   product: "https://u75w7w097c.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  // import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://cnvkm7svg9.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
